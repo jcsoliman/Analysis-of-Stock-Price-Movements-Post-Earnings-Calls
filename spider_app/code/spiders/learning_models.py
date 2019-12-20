f5442549331766fcd90b154b0a3526c5c4f252b1
Manpreet Padam <manpreet.padam@kodefury.com>
2019-12-20 12:36:00 -0500

    Updting TF model

---
+++ spider_app/code/spiders/learning_models.py
@@ -0,0 +1,11 @@
+from path import Path
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+
